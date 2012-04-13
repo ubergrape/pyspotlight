f075dd79f96f177a457d36518588a9f4f33b837c
Luis Nell <ln@nells.eu>
2012-04-13 14:46:01 +0200

    add tests for new exception handling

--- spotlight/tests.py
+++ spotlight/tests.py
@@ -4,24 +4,64 @@ try:
 except ImportError:
     SKIP_ORDERED_DICT_TESTS = True
     import sys
-    sys.stderr.write('Skipping _dict_cleanup due to OrderedDict not being available.\n')
+    sys.stderr.write('Skipping _dict_cleanup due to OrderedDict not being '
+                     'available.\n')
 
-from nose.tools import eq_
+from nose.tools import eq_, nottest, raises
 
-from spotlight import _convert_number, _dict_cleanup
+import spotlight
+
+
+@nottest
+def fake_request_post(self, *args, **kwargs):
+    class FakeResponse(object):
+        text = kwargs['headers']['fake_response']
+    return FakeResponse()
+spotlight.requests.post = fake_request_post
 
 
 def test_number_convert():
-    eq_(_convert_number('0'), 0)
-    eq_(_convert_number('0.2'), 0.2)
-    eq_(_convert_number(True), True)
-    eq_(_convert_number('evi'), 'evi')
+    eq_(spotlight._convert_number('0'), 0)
+    eq_(spotlight._convert_number('0.2'), 0.2)
+    eq_(spotlight._convert_number(True), True)
+    eq_(spotlight._convert_number('evi'), 'evi')
+
+
+@raises(spotlight.SpotlightException)
+def test_annotation_invalid_json():
+    spotlight.annotate('localhost', 'asdasdasd',
+                       headers={'fake_response': 'invalid json'})
+
+
+@raises(spotlight.SpotlightException)
+def test_missing_resources():
+    spotlight.annotate('localhost', 'asdasdasd',
+            headers={'fake_response': '{"Test": "Win"}'})
+
+
+@raises(spotlight.SpotlightException)
+def test_candidates_invalid_json():
+    spotlight.annotate('localhost', 'asdasdasd',
+                       headers={'fake_response': 'invalid json'})
+
+
+@raises(spotlight.SpotlightException)
+def test_missing_annotation():
+    spotlight.candidates('localhost', 'asdasdasd',
+            headers={'fake_response': '{"Test": "Win"}'})
+
+
+@raises(spotlight.SpotlightException)
+def test_missing_surfaceForms():
+    spotlight.candidates('localhost', 'asdasdasd',
+            headers={'fake_response': '{"annotation": {"Test": "Win"}}'})
 
 
 if not SKIP_ORDERED_DICT_TESTS:
     def test_dict_key_cleanup():
         dirty_dict = OrderedDict()
         dirty_dict['@dirty'] = 'value'
+        dirty_dict['@empty'] = None  # None values should be removed.
         dirty_dict['@recursive'] = OrderedDict()
         dirty_dict['@recursive']['tests'] = '1'
         dirty_dict['@recursive']['stuff'] = OrderedDict()
@@ -43,4 +83,5 @@ if not SKIP_ORDERED_DICT_TESTS:
         moremore['moar'] = True
         moar_iterable = [more, moremore]
         clean_dict['recursive']['stuff'] = moar_iterable
-        eq_(_dict_cleanup(dirty_dict, dict_type=OrderedDict), clean_dict)
+        eq_(spotlight._dict_cleanup(dirty_dict, dict_type=OrderedDict),
+            clean_dict)
