d3e2907fc882df181f756635562da341bca68426
Luis Nell <ln@nells.eu>
2013-08-12 20:19:57 +0200

    version bump.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ except ImportError:
 
 
 setup(name='pyspotlight',
-      version='0.6.5',
+      version='0.6.5.1',
       license='BSD',
       url='https://github.com/newsgrape/pyspotlight',
       packages=find_packages(),
